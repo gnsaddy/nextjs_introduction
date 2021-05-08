309f7f9b5f2a762ccf5eb3848c4b16fa5f213d05
Aditya Raj <aditya.x510@gmail.com>
2021-05-08 15:13:23 +0530

    $1

---
+++ pages/portfolio/[projectid].jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+export default function PortfolioProjectPage() {
+  const router = useRouter();
+  console.log("Portfolio Page path:", router.pathname);
+  console.log("Portfolio Page Id:", router.query);
+
+  return (
+    <div>
+      <h1>PortfolioProjectPage</h1>
+    </div>
+  );
+}
